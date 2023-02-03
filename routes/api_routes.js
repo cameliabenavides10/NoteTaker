2bb6f7dcabaff3f7335e9522fc2ac967b74795d7
cameliabenavides10 <cameliabenavides@gmail.com>
2023-02-02 18:30:48 -0600

    trying to add delete

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const uuid = require("../helpers/uuid");
 const utils = require("../helpers/fsUtils");
+const notes = require("../db/db.json");
 
 router.get("/notes", (req, res) => {
     utils.readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)))
@@ -17,4 +18,31 @@ router.post("/notes", (req, res) => {
     res.json("Success")
 });
 
+
+router.delete('/notes/:id', (req, res) => {
+    
+    if (req.params.id){
+        const notesId = req.params.id;
+        for (let i = 0; i < notes.length; i++) {
+            const currentNote = notes[i];
+            if (currentNote.id === notesId) {
+              currentNote.notes -= 1;
+              res.json(`Success`);
+              return;
+            }
+          }
+          res.status(404).json('Note ID not found');
+        } else {
+            res.status(400).send('Review ID not provided');
+          }
+      });
+
+
+
+// notes = (notes => notes.id !== id)
+//     }else {
+//         res.status(404).json({message:"cant find"})
+//     }
+//     res.json("Delete request successful");
+// });
 module.exports = router;
--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
