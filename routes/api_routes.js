7485e7da6767fa0655965aad8219ae7206e2ccb1
cameliabenavides10 <cameliabenavides@gmail.com>
2023-02-03 18:16:47 -0600

    spliced off given object, trying to rewrite it

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const uuid = require("../helpers/uuid");
 const utils = require("../helpers/fsUtils");
-const notes = require("../db/db.json");
+// const { fstat } = require("fs");
+const fs = require("fs");
+// const notes = require("../db/db.json");
 
 router.get("/notes", (req, res) => {
     utils.readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)))
@@ -20,24 +22,58 @@ router.post("/notes", (req, res) => {
 
 
 router.delete('/notes/:id', (req, res) => {
+    console.log(req.params.id);
+    utils.readFromFile("./db/db.json", "utf8")
+    .then(notes => {
+
+        // res.json(JSON.parse(notes) )
+        console.log(JSON.parse(notes));  
+    console.log(`This is object legnth ${JSON.parse(notes).length}`);
+    console.log(`This is param.id : ${req.params.id}`);
+    const newNotesInfo = JSON.parse(notes);
+   
+         for (let i = 0; i < newNotesInfo.length; i++) {
+           
+            if (newNotesInfo[i].id === req.params.id) {
+                const valueId = req.params.id;
+                const indexId= newNotesInfo.id;
+                console.log(`Hello ${newNotesInfo[i].id}`)
+
+                let newNewNote = newNotesInfo.splice(newNotesInfo[i].id, 1)
+console.log(newNewNote);
+                // function removeNote(newNotesInfo){
+                //    let whatever=  notes[i].id
+                //    console.log(whatever);
+                //         if ( req.params.id === whatever){
+                //             newNotesInfo.splice(indexId, 1);
+                //             return true;
+                //         }
+                //         return false;
+                // }
+                console.log(newNotesInfo);
+                // console.log(removeNote());
+                // const xId = newNotesInfo.filter(removeNote);
+                //    console.log(xId);
+                // newNotesInfo.splice()
+                // const index = newNotesInfo.indexof(req.params.id);
+                // newNotesInfo.splice(index, 1);
+
+                fs.writeFile('./db/db.json', JSON.stringify(newNotesInfo),
+                (err) => err? console.log(err): console.log("Your note has been deleted."));
+
+            res.status(400).json({ msg: "Successfully read` ${req.params.id}`" });
+          } else {
+            // notes.filter(notes => notes.id !== req.params.id);
+            console.log("After line 33")
+            
+          }
+        } 
+    })
     
-    if (req.params.id){
-        const notesId = req.params.id;
-        for (let i = 0; i < notes.length; i++) {
-            const currentNote = notes[i];
-            if (currentNote.id === notesId) {
-              currentNote.notes -= 1;
-              res.json(`Success`);
-              return;
-            }
-          }
-          res.status(404).json('Note ID not found');
-        } else {
-            res.status(400).send('Review ID not provided');
-          }
-      });
-
-
+    // const deleted = notes.some(notes.id === req.params.id);
+   res.json({msg :"not success"});
+});
+    
 
 // notes = (notes => notes.id !== id)
 //     }else {
