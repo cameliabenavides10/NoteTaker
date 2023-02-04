16462b34e74167a87d807717fb875dab04a5db5f
cameliabenavides10 <cameliabenavides@gmail.com>
2023-02-04 17:21:36 -0600

    fixed homepage

--- server.js
+++ server.js
@@ -13,17 +13,16 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // GET Route for feedback page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
