f5514096f68d0d28b2bfe066018f3f293ad9a40c
cameliabenavides10 <cameliabenavides@gmail.com>
2023-02-03 18:26:58 -0600

    fixed delete routing

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -35,50 +35,30 @@ router.delete('/notes/:id', (req, res) => {
          for (let i = 0; i < newNotesInfo.length; i++) {
            
             if (newNotesInfo[i].id === req.params.id) {
-                const valueId = req.params.id;
-                const indexId= newNotesInfo.id;
+              
                 console.log(`Hello ${newNotesInfo[i].id}`)
 
                 let newNewNote = newNotesInfo.splice(newNotesInfo[i].id, 1)
 console.log(newNewNote);
-                // function removeNote(newNotesInfo){
-                //    let whatever=  notes[i].id
-                //    console.log(whatever);
-                //         if ( req.params.id === whatever){
-                //             newNotesInfo.splice(indexId, 1);
-                //             return true;
-                //         }
-                //         return false;
-                // }
+            
                 console.log(newNotesInfo);
-                // console.log(removeNote());
-                // const xId = newNotesInfo.filter(removeNote);
-                //    console.log(xId);
-                // newNotesInfo.splice()
-                // const index = newNotesInfo.indexof(req.params.id);
-                // newNotesInfo.splice(index, 1);
+                
 
-                fs.writeFile('./db/db.json', JSON.stringify(newNotesInfo),
+                fs.writeFile('./db/db.json', JSON.stringify(newNotesInfo, null, 4),
                 (err) => err? console.log(err): console.log("Your note has been deleted."));
 
-            res.status(400).json({ msg: "Successfully read` ${req.params.id}`" });
+           
           } else {
-            // notes.filter(notes => notes.id !== req.params.id);
+           
             console.log("After line 33")
             
           }
         } 
     })
     
-    // const deleted = notes.some(notes.id === req.params.id);
-   res.json({msg :"not success"});
+    
+   res.json("success");
 });
     
 
-// notes = (notes => notes.id !== id)
-//     }else {
-//         res.status(404).json({message:"cant find"})
-//     }
-//     res.json("Delete request successful");
-// });
 module.exports = router;
