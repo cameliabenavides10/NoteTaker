2c34eda82259aca7c40206289319e24f814e4bfe
cameliabenavides10 <cameliabenavides@gmail.com>
2023-02-04 17:09:04 -0600

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/api_routes.js');
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
